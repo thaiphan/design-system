d84ff7c9b8b35fcd33b618f88a46585b9a7705f3
Marianne Borowiak <marianneb@nulogy.com>
2020-06-22 14:57:40 -0400

    change font to NotoSans SC for chinese locale

--- components/src/NDSProvider/NDSProvider.js
+++ components/src/NDSProvider/NDSProvider.js
@@ -6,30 +6,59 @@ import defaultTheme from "../theme";
 import i18n from "../i18n";
 import { LocaleContext } from "./LocaleContext";
 
-const Reset = createGlobalStyle({
-  body: {
-    margin: 0
-  }
+const Reset = createGlobalStyle(() => {
+  return {
+    body: {
+      margin: 0
+    }
+  };
 });
 
-const GlobalStyles = styled.div(({ theme }) => ({
-  color: theme.colors.black,
-  fontFamily: theme.fonts.base,
-  fontSize: theme.fontSizes.medium,
-  lineHeight: theme.lineHeights.base,
-  "-webkit-font-smoothing": "antialiased",
-  "-moz-osx-font-smoothing": "grayscale",
-  button: {
-    fontFamily: theme.fonts.base
-  },
-  "*": {
-    boxSizing: "border-box"
-  },
-  img: {
-    maxWidth: "100%",
-    height: "auto"
-  }
-}));
+const ModalStyleOverride = createGlobalStyle(({ theme, locale }) => {
+  const fontFamily = locale === "zh_CN" ? theme.fonts.sc : theme.fonts.base;
+  return {
+    ".ReactModal__Content": {
+      fontFamily,
+      button: {
+        fontFamily
+      },
+      input: {
+        fontFamily
+      },
+      textarea: {
+        fontFamily
+      }
+    }
+  };
+});
+
+const GlobalStyles = styled.div(({ theme, locale }) => {
+  const fontFamily = locale === "zh_CN" ? theme.fonts.sc : theme.fonts.base;
+  return {
+    color: theme.colors.black,
+    fontFamily,
+    fontSize: theme.fontSizes.medium,
+    lineHeight: theme.lineHeights.base,
+    "-webkit-font-smoothing": "antialiased",
+    "-moz-osx-font-smoothing": "grayscale",
+    "*": {
+      boxSizing: "border-box"
+    },
+    img: {
+      maxWidth: "100%",
+      height: "auto"
+    },
+    button: {
+      fontFamily
+    },
+    input: {
+      fontFamily
+    },
+    textarea: {
+      fontFamily
+    }
+  };
+});
 
 const NDSProvider = ({ theme, children, locale }) => {
   useEffect(() => {
@@ -37,8 +66,9 @@ const NDSProvider = ({ theme, children, locale }) => {
   }, [locale]);
   return (
     <LocaleContext.Provider value={{ locale }}>
-      <Reset />
-      <GlobalStyles theme={theme}>
+      <Reset theme={theme} locale={locale} />
+      <ModalStyleOverride theme={theme} locale={locale} />
+      <GlobalStyles theme={theme} locale={locale}>
         <I18nextProvider i18n={i18n}>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </I18nextProvider>
